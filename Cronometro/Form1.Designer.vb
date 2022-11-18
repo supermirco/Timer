30e360a7af285cfc4f6e2a5d1215158012178f89
mirco.lorizzo <mirco.lorizzo@gmail.com>
2022-11-18 15:25:21 +0100

    upload on github

---
+++ Cronometro/Form1.Designer.vb
@@ -0,0 +1,70 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla nell'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.BtnAdd = New System.Windows.Forms.Button()
+        Me.LblTimer = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'BtnAdd
+        '
+        Me.BtnAdd.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnAdd.Location = New System.Drawing.Point(93, 294)
+        Me.BtnAdd.Name = "BtnAdd"
+        Me.BtnAdd.Size = New System.Drawing.Size(192, 71)
+        Me.BtnAdd.TabIndex = 0
+        Me.BtnAdd.Text = "Inizia Il Timer"
+        Me.BtnAdd.UseVisualStyleBackColor = True
+        '
+        'LblTimer
+        '
+        Me.LblTimer.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblTimer.Location = New System.Drawing.Point(93, 142)
+        Me.LblTimer.Name = "LblTimer"
+        Me.LblTimer.Size = New System.Drawing.Size(192, 55)
+        Me.LblTimer.TabIndex = 1
+        Me.LblTimer.Text = "-"
+        Me.LblTimer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(378, 538)
+        Me.Controls.Add(Me.LblTimer)
+        Me.Controls.Add(Me.BtnAdd)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents BtnAdd As System.Windows.Forms.Button
+    Friend WithEvents LblTimer As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ Cronometro/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Dim x As Integer
+    Private Sub BtnAdd_Click(sender As System.Object, e As System.EventArgs) Handles BtnAdd.Click
+        x = 60
+        Timer1.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As System.Object, e As System.EventArgs) Handles Timer1.Tick
+        If x > 0 Then
+            LblTimer.Text = x
+            x = x - 1
+        Else
+            LblTimer.Text = x
+            Timer1.Stop()
+        End If
+    End Sub
+End Class
